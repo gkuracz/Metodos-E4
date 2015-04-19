972637c95adf7821e9823f1a3c7d7db1f3b6292e
suttonnico <suttonnico2@gmail.com>
2015-04-18 22:29:44 -0300

    Update mat.h

--- sourcefiles/mat.h
+++ sourcefiles/mat.h
@@ -1,53 +1,60 @@
-class mat 
+class mat
 {
-	public:
-		int N;
-		int M;
+public:
+	int N;
+	int M;
 
-		long double** matrix; // Main Matrix varirable
+	long double** matrix; // Main Matrix varirable
 
-		mat(int, int);//allocates memory for a m by n matrix
-		mat(char*); // Constructor that recieved csv
+	mat(int, int);//allocates memory for a m by n matrix
+	mat(char*); // Constructor that recieved csv
 
-		//if string inserted is "identityMatrix" it will create the identity matrix, must be squared.
-		mat(char * specificMatrix, int M, int N);
-		
-		~mat();
-		void print_mat(void); //Prints current Matrix
-		void print_mat_L(void); //Print LUDecomp, L matrix only if it exists.
-		void print_mat_U(void); //Print LUDecomp, U matrix only if it exists.
-		void print_thisMatrix(long double** thisMatrix, int M, int N); //Prints selected matrix
-		int mat_det(void);
-		
-		void product(mat& A, mat& B);
-		
-		
+	//if string inserted is "identityMatrix" it will create the identity matrix, must be squared.
+	mat(char * specificMatrix, int M, int N);
 
-		//Verifies if matrix is squared
-		bool isMatrixSquared(void);
+	~mat();
+	void print_mat(void); //Prints current Matrix
+	void print_mat_L(void); //Print LUDecomp, L matrix only if it exists.
+	void print_mat_U(void); //Print LUDecomp, U matrix only if it exists.
+	void print_thisMatrix(long double** thisMatrix, int M, int N); //Prints selected matrix
+	long double mat_det(void);
 
-		void swapRow(long double**, int row01, int row02); //Swaps the selected rows
-
-		void transpuesta(void); //Transposes de current matrix and saves it.
+	void product(mat& A, mat& B);
 
 
-		void LUdecomposition(void); // LUDecomposition, seperates them in matrixL and matrixU
-			long double** matrixL;
-			long double** matrixU;
 
-		void mat_inv(void);
-			long double** inv;
+	//Verifies if matrix is squared
+	bool isMatrixSquared(void);
 
-			void setVariablesToNull(void)
-			{
-				matrixL = NULL;
-				matrixU = NULL;
-				matrix  = NULL;
-				inv		= NULL;
-			}
+	void swapRow(long double**, int row01, int row02); //Swaps the selected rows
 
+	void transpuesta(void); //Transposes de current matrix and saves it.
+
+
+	void LUdecomposition(void); // LUDecomposition, seperates them in matrixL and matrixU
+	long double** matrixL;
+	long double** matrixU;
+
+	void mat_inv(void);
+	long double** inv;
+
+	void qr(void);
+	long double** Q;
+	long double** R;
+
+	void setVariablesToNull(void)
+	{
+		matrixL = NULL;
+		matrixU = NULL;
+		matrix = NULL;
+		inv = NULL;
+	}
+	void match_mats(long double **, int, int, long double **, int, int);
+
+	long double** get_col(long double **,int col,int M,int N);
+	long double ** new_mat(int M, int N);
 private:
-	
+
 
 	// Internal functions that are needed to transpose a matrix
 	void transpuestaCopyRowToColumn(long double** newMatrix, long double * rowToCopy, int rowNumber);
@@ -62,4 +69,5 @@ private:
 
 	void createIdentityMatrix(void);
 	
+
 };
