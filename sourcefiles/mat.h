a2c25b79306db5c984f0dc7cbf4f98db220bef2a
suttonnico <suttonnico2@gmail.com>
2015-04-21 13:12:03 -0300

    Update mat.h

--- sourcefiles/mat.h
+++ sourcefiles/mat.h
@@ -1,12 +1,22 @@
+
+void solveLeastSquares(long double **matrixQ, long double **matrixR, long double **matrixB, long double ** matrixSolution, int M, int N);
+
+long double** createTimeVectorMatrix(long double ** matrixA, long double ** timeVector, int totalRows);
+
+long double ** standarizationOfTimeValues(long double ** tMatrix, int totalRows);
+
+long double ** applyLogarithmToDataValues(long double ** sMatrix, int totalRows);
+
+
 class mat
 {
 public:
 	int N; // columna
 	int M; // fila
 
-	friend void solveLeastSquares(double**matrixQ, double **matrixR, double **matrixB, double * matrixSolution, int M, int N);
+	friend void solveLeastSquares(long double**matrixQ,long double **matrixR,long double **matrixB,long double * matrixSolution, int M, int N);
 
-	double** matrix; // Main Matrix varirable
+	long double** matrix; // Main Matrix varirable
 
 	mat(int, int);//allocates memory for a m by n matrix
 	mat(char*); // Constructor that recieved csv
@@ -20,11 +30,11 @@ public:
 	void print_mat_U(void); //Print LUDecomp, U matrix only if it exists.
 	void print_mat_Q(void); //Print QRDecomp, Q matrix only if it exists.
 	void print_mat_R(void); //Print QRDecomp, R matrix only if it exists.
-	void print_thisMatrix(double** thisMatrix, int M, int N); //Prints selected matrix
-	double mat_det(void);
+	void print_thisMatrix(long double** thisMatrix, int M, int N); //Prints selected matrix
+	long double mat_det(void);
 	
 
-	double** mat::product(double** A, double** B, int AM, int AN, int BM, int BN);
+	long double** mat::product(long double** A,long double** B, int AM, int AN, int BM, int BN);
 
 	mat& mat::operator=(mat B)
 	{
@@ -49,23 +59,23 @@ public:
 	//Verifies if matrix is squared
 	bool isMatrixSquared(void);
 
-	void swapRow(double**, int row01, int row02); //Swaps the selected rows
+	void swapRow(long double**, int row01, int row02); //Swaps the selected rows
 
 	void transpuesta(void); //Transposes de current matrix and saves it.
 
 
 	void LUdecomposition(void); // LUDecomposition, seperates them in matrixL and matrixU
-	double** matrixL;
-	double** matrixU;
+	long double** matrixL;
+	long double** matrixU;
 
 	void mat_inv(void);
-	double** inv;
+	long double** inv;
 
 	void qr(void);
-	double** Q;
-	double** R;
+	long double** Q;
+	long double** R;
 
-	double ** transpose_of_col(double** col, int L);
+	long double ** transpose_of_col(long double** col, int L);
 
 	void setVariablesToNull(void)
 	{
@@ -74,31 +84,31 @@ public:
 		matrix = NULL;
 		inv = NULL;
 	}
-	void match_mats(double **, int, int, double **, int, int);
+	void match_mats(long double **, int, int, long double **, int, int);
 
-	double** get_col(double **,int col,int M,int N);
-	double ** new_mat(int M, int N);
-	void f_get_col(double **A, int c, int M, int N, double **col);
-	void f_get_trans(double **A, int M, int N, double **col);
-	void kill(double**, int, int);
-	void cleanMatrix(double **matrix, int M, int N);
+	long double** get_col(long double **, int col, int M, int N);
+	long double ** new_mat(int M, int N);
+	void f_get_col(long double **A, int c, int M, int N, long double **col);
+	void f_get_trans(long double **A, int M, int N, long double **col);
+	void kill(long double**, int, int);
+	void cleanMatrix(long double **matrix, int M, int N);
 
 	void QRDecomposition(void);
 private:
 
 
 	// Internal functions that are needed to transpose a matrix
-	void transpuestaCopyRowToColumn(double** newMatrix, double * rowToCopy, int rowNumber);
-	void transposeSetThisNewMatrix(double** newMatrix);
+	void transpuestaCopyRowToColumn(long double** newMatrix, long double * rowToCopy, int rowNumber);
+	void transposeSetThisNewMatrix(long double** newMatrix);
 	
-	double norm2ofvector(double**,int L);
+	long double norm2ofvector(long double**, int L);
 	//PASS AS COLUMN
 	
 	void createLUDecompMatrix(void);
 
 	//Creates a matrix MxN, it will return the pointer to the matrix ** 
-	double ** newAuxMatrixSamesize(void);
-	double ** newAuxIdentityMatrix(void);
+	long double ** newAuxMatrixSamesize(void);
+	long double ** newAuxIdentityMatrix(void);
 
 
 	void createIdentityMatrix(void);
@@ -106,10 +116,3 @@ private:
 
 };
 
-void solveLeastSquares(double **matrixQ, double **matrixR, double **matrixB, double ** matrixSolution, int M, int N);
-
-double** createTimeVectorMatrix(double ** matrixA, double ** timeVector, int totalRows);
-
-double ** standarizationOfTimeValues(double ** tMatrix, int totalRows);
-
-double ** applyLogarithmToDataValues(double ** sMatrix, int totalRows);
